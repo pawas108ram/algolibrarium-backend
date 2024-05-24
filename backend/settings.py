22a0707404134b4c23c85066cdede9110019265d
pawas goyal <pawas48ram@gmail.com>
2024-05-24 11:10:13 +0530

    changed the settings.py to allow local error testing

--- backend/settings.py
+++ backend/settings.py
@@ -147,7 +147,7 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ALLOWED_ORIGINS = [
-   "**","https://algolibrarium.vercel.app"
+   "**","https://algolibrarium.vercel.app","http://localhost:3000"
 ]
 
 
